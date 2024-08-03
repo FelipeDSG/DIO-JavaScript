705116d74464e5f47019cbcc38d98761c84d33f1
Felipe Grijó <felipessantoss@hotmail.com>
2024-08-03 20:16:56 -0300

    Finalizando o projeto

--- 12-projeto-pokemon/assets/js/main.js
+++ 12-projeto-pokemon/assets/js/main.js
@@ -1,36 +1,66 @@
 
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => ` <li class="type">${typeSlot.type.name}</li>`)
-}
+const maxRecords = 151
+const limit = 5
+let offset = 0
 
-function convertPokemonToLi(pokemon) {
-    return `
-    <li class="pokemon">
-                <span class="number">#${pokemon.order}</span>
+// function convertPokemonToLi(pokemon) {
+//     return `
+//     <li class="pokemon ${pokemon.type}">
+//                 <span class="number">#${pokemon.number}</span>
+//                 <span class="name">${pokemon.name}</span>
+
+//                 <div class="detail">
+
+//                     <ol class="types" >
+//                         ${pokemon.types.map((type) => ` <li class="type ${type}" >${type}</li>`).join('')}
+//                     </ol>
+
+//                     <img src="${pokemon.photo}"
+//                         alt="${pokemon.name}">
+//                 </div>
+//             </li>
+//             `
+// }
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>
+            `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
 
-                    <ol class="types">
-                        ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    <ol class="types" >
+                        ${pokemon.types.map((type) => ` <li class="type ${type}" >${type}</li>`).join('')}
                     </ol>
 
-                    <img src="${pokemon.sprites.other.dream_world.front_default}"
+                    <img src="${pokemon.photo}"
                         alt="${pokemon.name}">
                 </div>
             </li>
             `
+        ).join('')
+
+        pokemonList.innerHTML += newHtml
+    })
 }
+loadPokemonItens(offset, limit)
 
-const pokemonList = document.getElementById('pokemonList')
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = newHtml
+    const qtdRecordsNextPage = offset + limit
 
+    if (qtdRecordsNextPage >= maxRecords) {
+        const newLimit =  maxRecords - offset 
+        loadPokemonItens(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    }else{
+        loadPokemonItens(offset, limit)
+    }
 })
-
-
-
-//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg
